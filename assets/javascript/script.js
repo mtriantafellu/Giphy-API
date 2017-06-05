99c0350012ffe19a5eab92ad3e95b4462f663d10
matt <mtriantafellu@yahoo.com>
2017-06-04 21:11:03 -0400

    now search results not returning

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,30 +7,117 @@
  
 //API Key:
 
-var APIKey = "dc6zaTOxFJmzC";
-var searchTerm = "";
+
+
+//Giphy API URL + API Key:
+
+//var queryURLbase = "http://api.giphy.com/v1/gifs/random&tag=" + searchTerm + "&api_key=" + APIkey + "&limit=10";
+
+//var queryURLBase = "http://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + searchTerm;
+
+$(document).ready(function(){
+  console.log("ready!");
+
+var APIkey = "dc6zaTOxFJmzC";
+
 var cars = [
   "Ford Mustang",
   "Chevy Camero",
   "Dodge Viper",
 ];
 
-//Giphy API URL + API Key:
-var queryURLBase = "http://api.giphy.com/v1/gifs/random?api_key=" + APIKey + "&tag=" + searchTerm;
-
-$(document).ready(function(){
-  console.log("ready!");
 //Pressing Enter now initializes the search button
-$("#input").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#searchBtn").click();
-    }
-});
+//$("#input").keyup(function(event){
+    //if(event.keyCode == 13){
+        //$("#searchBtn").click();
+    //}
+//});
 //End Enter Button
 
-//On.Button Click:
+function displayCarInfo() {
+
+var searchTerm = "";
+var queryURLBase = "http://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + searchTerm;
+
+  var car = $(this).attr(".car");
+  var queryURL = queryURLbase + searchTerm;
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    })
+
+    .done(function(response) {
+      
+      console.log("done response", response);
+
+      var carDiv = $("<div class='car'>");
+
+      var rating = response.data.Rated;
+
+      var p0ne = $("<p>").text("Rating:" + rating);
+
+      carDiv.append(pOne);
+
+      var imageURL = response.data.image_original_url;
+
+      var image = $("<img>");
+
+      image.attr("src", imageURL);
+
+      image.attr("alt", "image");
+
+      $("#image").prepend(image);
+
+    });  //End .done function
+
+// End Button Click
+
+}// End Function displayCarInfo
+
+function renderButtons() {
+  $("#divBtn").empty();
+
+  for (var i = 0; i < cars.length; i++) {
+
+    var a = $("<button>");
+
+    a.addClass("car");
+    a.attr("data-name", cars[i]);
+    a.text(cars[i]);
+    $("divBtn").append(a);
+
+  };
+};
+
 $("#searchBtn").on("click", function(event) {
 
+  event.preventDefault();
+
+  var car = $("input").val().trim();
+
+  cars.push(car);
+
+  renderButtons();
+
+  console.log("ready2");
+});
+
+$(document).on("click", ".car", displayCarInfo);
+
+renderButtons();
+
+});// End document.ready
+
+
+
+
+
+
+// Previous!!!
+/*
+//On.Button Click:
+$("#searchBtn").on("click", function(event) {
 
   event.preventDefault();
 
@@ -38,7 +125,7 @@ $("#searchBtn").on("click", function(event) {
   //  $("#well-section").empty();
 
   searchTerm = $("input").val().trim();
-  var queryURL = queryURLBase + searchTerm;
+  var queryURL = queryURLbase;
 
     $.ajax({
       url: queryURL,
@@ -87,8 +174,11 @@ $("#searchBtn").on("click", function(event) {
   cars.push(car);
 
   renderButtons();
+
+  console.log("ready2");
 });
 
 renderButtons();
 
 });// End document.ready
+*/
